9e2e0a6a0733022d81b4d6f98c21df701697f5c8
crmulent <kingcapacio47@gmail.com>
2024-05-19 03:10:33 +0800

    Finalized program

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from src.encrypt import encrypt_image
 from src.decrypt import decrypt_image
+import argparse
 
-if __name__=="__main__":
-    image = "orig.jpg"
-    key = 12345
-    encrypt_image(image, key)
-    decrypt_image("orig_encrypted.png", key)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Encrypt/decrypt an image using XOR method.',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('-e', type=str, metavar='image', help='image to encrypt')
+    parser.add_argument('-d', type=str, metavar='image', help='image to decrypt')
+    parser.add_argument('-k', type=str, metavar='key', help='key to use')
+    image_to_encrypt = parser.parse_args().e
+    image_to_decrypt = parser.parse_args().d
+    key = parser.parse_args().k
+
+    if not (image_to_encrypt or image_to_decrypt):
+        print('usage: password_checker.py [-h] [-e image] [-d image] -k key')
+        return 1
+    
+    if image_to_encrypt:
+        encrypt_image(image_to_encrypt, key)
+    elif image_to_decrypt:
+        decrypt_image(image_to_decrypt, key)
+
+
+if __name__ == '__main__':
+    main()
